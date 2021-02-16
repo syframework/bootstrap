fb9a631d115740188c79507f87e46a85746e112d
syone <syone7@gmail.com>
2021-02-16 19:57:45 +0100

    remove specific use case

--- src/Lib/Mail/HtmlTranslate.php
+++ src/Lib/Mail/HtmlTranslate.php
@@ -80,48 +80,13 @@ abstract class HtmlTranslate extends \Sy\Bootstrap\Lib\Mail {
 
 	public function send() {
 		$this->init();
-		$company = explode('.', explode('@', $this->getTo())[1])[0];
-		if (in_array($company, ['hotmail', 'live', 'outlook'])) {
-			$this->mailjet();
-		} else {
-			$this->mail();
-		}
+		$this->mail();
 	}
 
 	private function mail() {
-		$company = explode('.', explode('@', $this->getTo())[1])[0];
-		if ($company === 'free') $this->setCc('nospam@nospam.proxad.net');
-		//$this->setContentType('multipart/alternative; boundary=' . $this->separator);
 		$this->addTextBody();
 		$this->addHtmlBody();
 		parent::send();
-	}
-
-	private function tipimail() {
-		$tipimail = new \Tipimail\Tipimail(TIPIMAIL_USER, TIPIMAIL_KEY);
-		$data = new \Tipimail\Messages\Message();
-		$data->addTo($this->getTo(), null);
-		$data->setFrom(TEAM_MAIL, PROJECT);
-		$data->setSubject($this->getSubject());
-		$data->setHtml($this->getHtml());
-		$data->setText($this->getText());
-		$tipimail->getMessagesService()->send($data);
-	}
-
-	private function mailjet() {
-		$mj = new \Mailjet\Client(MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE);
-		$body = [
-			'FromEmail' => TEAM_MAIL,
-			'FromName'  => PROJECT,
-			'Subject'   => $this->getSubject(),
-			'Text-part' => $this->getText(),
-			'Html-part' => $this->getHtml(),
-			'To'        => $this->getTo()
-		];
-		$response = $mj->post(\Mailjet\Resources::$Email, ['body' => $body]);
-		if (!$response->success()) {
-			$this->mail();
-		}
 	}
 
 	private function addTextBody() {

ea00e1fcedb756bacaea143b165092c7985c6d46
syone <syone7@gmail.com>
2021-09-13 23:20:03 +0200

    share button

--- src/Component/Share/Button.php
+++ src/Component/Share/Button.php
@@ -5,11 +5,8 @@ use Sy\Bootstrap\Component\Modal\Button as ModalButton;
 
 class Button extends ModalButton {
 
-	private $url;
-
 	public function __construct($label = '', $url = '', $icon = 'share-alt', $color = 'secondary', $width = '100', $size = '', $title = '') {
-		parent::__construct('shareModal', $label, $icon, $color, $width, $size, $title);
-		$this->url = $url;
+		parent::__construct('shareModal', $label, $icon, $color, $width, $size, $title, ['data-url' => $url]);
 	}
 
 	public function __toString() {
@@ -20,7 +17,7 @@ class Button extends ModalButton {
 	private function init() {
 		$this->addTranslator(LANG_DIR);
 		$dialog = $this->getDialog();
-		$dialog->setBody(new Buttons($this->url));
+		$dialog->setBody(new Buttons(''));
 		$this->addJsCode(__DIR__ . '/Button.js');
 	}
 

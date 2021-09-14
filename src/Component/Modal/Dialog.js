23984dbd9b261e9c7ef8c6a95f1540cea4a06626
syone <syone7@gmail.com>
2021-09-14 15:19:45 +0200

    fix dialog title

--- src/Component/Modal/Dialog.js
+++ src/Component/Modal/Dialog.js
@@ -1,11 +1,11 @@
 (function() {
 	$('.modal').appendTo('body');
 
-	$('.modal').on('show.bs.modal', function (e) {
-		var button = (e.relatedTarget === undefined) ? $('button[data-bs-target="#' + $(this).attr('id') + '"]') : $(e.relatedTarget);
+	$('body').on('show.bs.modal', '.modal', function (e) {
+		var button = (e.relatedTarget === undefined) ? $('[data-bs-target="#' + $(this).attr('id') + '"]') : $(e.relatedTarget);
 		var title = $(this).find('.modal-title');
 		if (title.text() !== '') return;
-		title.html(button.html());
+		title.html((button.text().trim() === '') ? button.html() + button.data('bs-title') : button.html());
 	});
 
 	$('.modal').each(function () {
--- src/Component/Share/Button.js
+++ src/Component/Share/Button.js
@@ -1,12 +1,8 @@
-(function() {
-	$('body').on('show.bs.modal', '#shareModal', function (event) {
-		var button = $(event.relatedTarget);
-		var url = button.data('url');
-		if (url !== undefined && url.length > 0) {
-			$(this).find('.share-url').val(url);
-			$(this).find('.popup').data('url', url);
-		}
-		var title = (button.text().trim() === '') ? button.html() + button.data('bs-title') : button.html();
-		$(this).find('.modal-title').html(title);
-	});
-})();
+$('body').on('show.bs.modal', '#shareModal', function (event) {
+	var button = $(event.relatedTarget);
+	var url = button.data('url');
+	if (url !== undefined && url.length > 0) {
+		$(this).find('.share-url').val(url);
+		$(this).find('.popup').data('url', url);
+	}
+});

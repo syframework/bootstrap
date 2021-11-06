4466e8199bb553142644569c51a63173a01e23f4
syone <syone7@gmail.com>
2021-11-06 12:49:21 +0100

    fix indent

--- src/Lib/Mail/Button.php
+++ src/Lib/Mail/Button.php
@@ -3,27 +3,27 @@ namespace Sy\Bootstrap\Lib\Mail;
 
 class Button extends \Sy\Component\WebComponent {
 
-    private $label;
-    private $url;
+	private $label;
+	private $url;
 
-    public function __construct($label, $url) {
-        parent::__construct();
-        $this->label = $label;
-        $this->url = $url;
-    }
+	public function __construct($label, $url) {
+		parent::__construct();
+		$this->label = $label;
+		$this->url = $url;
+	}
 
-    public function __toString() {
-        $this->init();
-        return parent::__toString();
-    }
+	public function __toString() {
+		$this->init();
+		return parent::__toString();
+	}
 
-    private function init() {
-        $this->addTranslator(LANG_DIR . '/mail');
-        $this->setTemplateFile(__DIR__ . '/Button.html');
-        $this->setVars([
-            'URL'   => $this->url,
-            'LABEL' => $this->_($this->label)
-        ]);
-    }
+	private function init() {
+		$this->addTranslator(LANG_DIR . '/mail');
+		$this->setTemplateFile(__DIR__ . '/Button.html');
+		$this->setVars([
+			'URL'   => $this->url,
+			'LABEL' => $this->_($this->label)
+		]);
+	}
 
 }

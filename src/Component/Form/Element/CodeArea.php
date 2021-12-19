f4064b22e694d63c1f062f9294ccdf55da11f191
syone <syone7@gmail.com>
2021-12-19 23:54:48 +0100

    upgrade code mirror version

--- src/Component/Form/Element/CodeArea.php
+++ src/Component/Form/Element/CodeArea.php
@@ -3,7 +3,7 @@ namespace Sy\Bootstrap\Component\Form\Element;
 
 class CodeArea extends \Sy\Component\Html\Form\Textarea {
 
-	private $cdn = 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.0';
+	private $cdn = 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.64.0';
 
 	private $theme = 'default';
 

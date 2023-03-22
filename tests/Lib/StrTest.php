b69cb26b26ae44e01f6a5d25ccc0cbe417bd2e30
Syone <syone7@gmail.com>
2023-03-22 11:33:36 +0100

    add unit tests for Str::camlToSnake

--- tests/Lib/StrTest.php
+++ tests/Lib/StrTest.php
@@ -76,4 +76,26 @@ class StrTest extends TestCase {
 		);
 	}
 
+	public function testCamlToSnake() {
+		$this->assertEquals('simple_test', Str::camlToSnake('simpleTest'));
+		$this->assertEquals('easy', Str::camlToSnake('easy'));
+		$this->assertEquals('html', Str::camlToSnake('HTML'));
+		$this->assertEquals('simple_xml', Str::camlToSnake('simpleXML'));
+		$this->assertEquals('start_middle_end', Str::camlToSnake('startMIDDLEEnd'));
+		$this->assertEquals('a_string', Str::camlToSnake('AString'));
+		$this->assertEquals('some4_numbers234', Str::camlToSnake('Some4Numbers234'));
+		$this->assertEquals('test123_string', Str::camlToSnake('TEST123String'));
+		$this->assertEquals('hello_world', Str::camlToSnake('hello_world'));
+		$this->assertEquals('hello___world', Str::camlToSnake('hello___world'));
+		$this->assertEquals('_hello_world_', Str::camlToSnake('_hello_world_'));
+		$this->assertEquals('hello_world', Str::camlToSnake('HelloWorld'));
+		$this->assertEquals('hello_world_foo', Str::camlToSnake('HelloWorldFoo'));
+		$this->assertEquals('hello_world', Str::camlToSnake('hello_World'));
+		$this->assertEquals('hello-world', Str::camlToSnake('hello-world'));
+		$this->assertEquals('my_html_fi_le', Str::camlToSnake('myHTMLFiLe'));
+		$this->assertEquals('a_ba_ba_b', Str::camlToSnake('aBaBaB'));
+		$this->assertEquals('ba_ba_ba', Str::camlToSnake('BaBaBa'));
+		$this->assertEquals('lib_c', Str::camlToSnake('libC'));
+	}
+
 }

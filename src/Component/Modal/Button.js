b418dffa334cba2fa07eee3e790775067988ddc7
Syone <syone7@gmail.com>
2023-07-25 15:55:22 +0200

    fix modal button dialog

--- src/Component/Modal/Button.js
+++ src/Component/Modal/Button.js
@@ -1,13 +1,17 @@
 (function() {
-	$('body').on('mousedown', '[data-bs-toggle="modal"]', function () {
+	$('body').on('mousedown', '[data-bs-toggle="modal"][data-dialog]', function () {
 		var id = $(this).data('bs-target');
 		if ($(id).length > 0) return;
 		$('body').append($(this).data('dialog'));
 	});
 
-	$('button[data-bs-toggle="modal"]').each(function () {
-		var id = $(this).data('bs-target');
-		if ($(id).length > 0) return;
-		$('body').append($(this).data('dialog'));
+	$('button[data-bs-toggle="modal"][data-dialog]').each(function () {
+		if ($($(this).data('dialog')).find('div.alert-danger').length > 0) {
+			var id = $(this).data('bs-target');
+			if ($(id).length == 0) {
+				$('body').append($(this).data('dialog'));
+			}
+			$(this).click();
+		}
 	});
 })();

90ab50288029a70b795621fe7bbe9a3734a42acf
Syone <syone7@gmail.com>
2023-08-01 20:34:52 +0200

    move postInit in mount function

--- src/Component/Form/Element/CodeArea.php
+++ src/Component/Form/Element/CodeArea.php
@@ -14,6 +14,8 @@ class CodeArea extends \Sy\Component\Html\Form\Textarea {
 	public function __construct() {
 		parent::__construct();
 		$this->preInit();
+
+		$this->mount(fn () => $this->postInit());
 	}
 
 	public function setTheme($theme) {
@@ -55,11 +57,6 @@ class CodeArea extends \Sy\Component\Html\Form\Textarea {
 			'PLACEHOLDER'  => $this->getAttribute('placeholder'),
 		]);
 		$this->addJsCode($js, ['position' => WebComponent::JS_TOP]);
-	}
-
-	public function __toString() {
-		$this->postInit();
-		return parent::__toString();
 	}
 
 }

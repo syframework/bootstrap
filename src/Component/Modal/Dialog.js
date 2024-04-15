e09efcccd7c4a391e58e54793bae71453b542b18
Syone <syone7@gmail.com>
2024-04-15 22:30:15 +0200

    fix dialog

--- src/Component/Modal/Dialog.js
+++ src/Component/Modal/Dialog.js
@@ -2,6 +2,7 @@
 	document.body.addEventListener('show.bs.modal', function (e) {
 		var target = e.relatedTarget || document.querySelector('[data-bs-target="#' + e.target.id + '"]');
 		var title = e.target.querySelector('.modal-title');
+		if (!title) return;
 		if (title.textContent !== '') return;
 		title.innerHTML = (target.textContent.trim() === '') ? target.innerHTML + target.dataset.bsTitle : target.innerHTML;
 	}, true);

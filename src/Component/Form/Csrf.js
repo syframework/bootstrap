3027bdab949829cbd3b4d836278e229bb03ece2a
Syone <syone7@gmail.com>
2024-06-24 12:57:22 +0200

    update crsf on form submit error

--- src/Component/Form/Csrf.js
+++ src/Component/Form/Csrf.js
@@ -1,11 +1,15 @@
 (function() {
-	setInterval(function() {
+	function updateCsrf() {
 		const location = new URL('{URL}', window.location.origin);
-		location.searchParams.set('ts', new Date().getTime());
+		location.searchParams.set('ts', Date.now());
 		fetch(location.href).then(response => response.json()).then(data => {
 			document.querySelectorAll('input[name=__csrf').forEach(input => {
 				input.value = data.csrf;
 			});
 		});
-	}, 1200000);
+	}
+
+	window.addEventListener('csrf', updateCsrf);
+
+	setInterval(updateCsrf, 1200000);
 })();

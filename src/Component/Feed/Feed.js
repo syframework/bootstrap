36410d657d3c8096fb8602c00150216aaa6993cc
Syone <syone7@gmail.com>
2025-02-14 23:39:53 +0100

    fix feed ajax url with array search params

--- src/Component/Feed/Feed.js
+++ src/Component/Feed/Feed.js
@@ -1,7 +1,7 @@
 (function () {
 
 	document.body.addEventListener('click', function (event) {
-		var button = null;
+		let button = null;
 		if (event.target.classList.contains('feed-next-page-button')) {
 			button = event.target;
 		}
@@ -11,28 +11,37 @@
 		if (!button) return;
 		button.classList.remove('feed-next-page-button');
 		button.disabled = true;
-		var spinner = document.createElement('span');
+		let spinner = document.createElement('span');
 		spinner.className = 'spinner-border spinner-border-sm';
 		spinner.setAttribute('role', 'status');
 		spinner.setAttribute('aria-hidden', 'true');
-		var biIcon = button.querySelector('.bi');
+		let biIcon = button.querySelector('.bi');
 		if (biIcon) {
 			biIcon.parentNode.replaceChild(spinner, biIcon);
 		}
-		var lastId = button.previousElementSibling ? button.previousElementSibling.dataset.id : null;
+		let lastId = button.previousElementSibling ? button.previousElementSibling.dataset.id : null;
 		if (!lastId) {
 			lastId = button.nextElementSibling ? button.nextElementSibling.dataset.id : null;
 		}
 		if (!lastId) {
 			lastId = button.dataset.start;
 		}
-		var params = JSON.parse(button.dataset.params);
+		const params = JSON.parse(button.dataset.params);
 		params['class'] = button.dataset.class;
 		params['last'] = lastId;
-		var location = new URL(button.dataset.location, window.location.origin);
-		Object.entries(params).forEach(([key, value]) => {
+		const location = new URL(button.dataset.location, window.location.origin);
+		const addParam = (key, value) => {
 			if (value === null) return;
-			location.searchParams.set(key, value);
+			if (typeof value === 'object') {
+				Object.entries(value).forEach(([nestedKey, nestedValue]) => {
+					addParam(`${key}[${nestedKey}]`, nestedValue);
+				});
+			} else {
+				location.searchParams.set(key, value);
+			}
+		};
+		Object.entries(params).forEach(([key, value]) => {
+			addParam(key, value);
 		});
 		fetch(location.href)
 			.then(response => response.text())
